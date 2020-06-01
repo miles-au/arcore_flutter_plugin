a94a5a498fe4233328f54ddac89e24a3abfa3715
Miles Au <miles.d.au@gmail.com>
2020-06-01 15:00:01 -0400

    fixed custom sfb object

--- example/lib/screens/assets_object.dart
+++ example/lib/screens/assets_object.dart
@@ -106,17 +106,15 @@ class ListObjectSelection extends StatefulWidget {
 
 class _ListObjectSelectionState extends State<ListObjectSelection> {
   List<String> gifs = [
-    "assets/halloween.gif",
     "assets/TocoToucan.gif",
     "assets/AndroidRobot.gif",
     "assets/ArcticFox.gif",
   ];
 
   List<String> objectsFileName = [
-    "halloween.sfb",
-    "toucan.sfb",
-    "andy.sfb",
-    "artic_fox.sfb"
+    "TocoToucan.sfb",
+    "Andy.sfb",
+    "ArcticFox_Posed.sfb"
   ];
 
   String selected;
